ebc35029b33a7f8a7fa0a9f437430341923588f5
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-11-19 22:05:06 +0000

    completedToThePost

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import layout from "../layout";
+
 
 export const Card = () => {
     const { store, actions } = useContext(Context)
